1ce8130864e742a4502105ec3bbcbd93d04a8f53
Kerice Forde <fordekerice@gmail.com>
2023-11-05 13:59:03 -0500

    Added edited files

--- scripts.js
+++ scripts.js
@@ -1,1 +1,20 @@
-/* Add your JavaScript to this file */
+/* Add your JavaScript to this file*/
+const newsForm=document.querySelector("form");
+
+const message= document.getElementsByClassName("message");
+ 
+
+//Adding functionality to the form and submit button in the newsletter section//
+newsForm.addEventListener("submit", function (e){
+    e.preventDefault();
+    
+    const email= document.getElementById("email").value;
+    
+    if (email.trim()===""){
+        message.innerHTML ="Please enter a valid email address." ;
+       
+     }
+    else{
+        message.innerHTML =`Thank you! Your email address ${email} has been added to our mailing list!`;
+    }
+} )
